4fbf4cdc9d5d5247fea5887a419688c31b57999d
gauravthorath <gauravjobs25@gmail.com>
2022-01-31 00:23:43 +0530

    Added AppBar and profile image on app component and removed other extra files

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import React from "react";
+import Box from "@mui/material/Box";
 import "./App.css";
+import Container from "@mui/material/Container";
+import ResponsiveAppBar from "./AppBar";
+import profile from "./gaurav.png"; // relative path to image
+import Paper from "@mui/material/Paper";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        Learn React
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <Box sx={{ bgcolor: "#e2d6f3", height: "100vh" }}>
+        <Paper elevation={3} sx={{ width: 230, height: 260 }}>
+          {" "}
+          <img src={profile} alt="Profile"></img>
+          Gaurav Thorat <br />
+          Senior Frontend Engineer
+        </Paper>
+      </Box>
+    </Container>
   );
 }
 
